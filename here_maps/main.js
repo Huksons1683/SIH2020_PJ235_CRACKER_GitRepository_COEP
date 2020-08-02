4b3c9fea6ff40e4b5f4ba478b89ded998da39243
Rahul kumar <rahulparida933@gamil.com>
2020-08-02 16:36:12 +0530

    location updated

--- here_maps/main.js
+++ here_maps/main.js
@@ -38,11 +38,11 @@ function findNearestMarker(coords) {
 	apikey: window.apikey
   });
   var defaultLayers = platform.createDefaultLayers();
-  
+  lat:28.37575
   //Step 2: initialize a map
   var map = new H.Map(document.getElementById('map'),
 	defaultLayers.vector.normal.map,{
-	center: {lat:  20.318215, lng: 85.824561},
+	center: {lat: 28.41352, lng: 77.3467},
 	zoom: 16,
 	pixelRatio: window.devicePixelRatio || 1
   });
@@ -73,12 +73,16 @@ function findNearestMarker(coords) {
 	{  lat:28.37383 , lng:77.35571},
 	{  lat:28.34133 , lng:77.19565},
 	{ lat:28.33198 , lng:77.20968},
-	{ lat:28.38982 , lng:77.36067}];
+	{ lat:28.38982 , lng:77.36067},{lat:28.41352, lng:77.3467},{lat:28.44251, lng:77.33032},
+	{lat:28.41679, lng:77.34044},{lat:28.48027, lng:77.32948},{lat:28.37972, lng:77.33846},{lat:28.41952, lng:77.33725},
+	{lat:28.37329, lng:77.32356},{lat:28.3515, lng:77.28434},{lat:28.33822,lng:77.29707},{lat:28.39858, lng:77.19657},
+	{lat:28.38037, lng:77.22834},{lat:28.38644, lng:77.27999},{lat:28.40791, lng:77.27508},{lat:28.34034, lng:77.28033},
+	{lat:28.34898, lng:77.32594},{lat:28.4772, lng:77.29582}];
 
      // lat:28.27895 , lng:77.28822
 	// lat:28.45053 , lng:77.28352
 	// lat:28.42011 , lng:77.34608
-	  
+	
   //Create the svg mark-up
   var svgMarkup = '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">' +
 	  '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" height="22" />' +
